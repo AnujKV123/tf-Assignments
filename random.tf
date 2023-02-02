96e145569964abb083617a275ac6868836dc11e4
Anuj <anuj8ranchi@gmail.com>
2023-02-02 20:32:45 +0530

    changes on random.tf file

--- random.tf
+++ random.tf
@@ -1,12 +1,9 @@
 #  terraform random ******************************************
-locals {
-  xyz = "Hi how are you"
-}
 
 resource "local_file" "fooLz" {
   # filename = "${random_id.random-file-name.hex}.txt"
   filename = "${random_id.random-file-name.hex}.html"
-  content  = local.xyz
+  content  = "Hi how are you"
 }
 
 resource "random_id" "random-file-name" {
